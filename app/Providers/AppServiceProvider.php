0787f845c1bf5f02d497d022e4c5f010c21b72f4
minimarket32 <minos3217@gmail.com>
2026-04-01 18:38:21 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Importamos la clase URL
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        /**
+         * Forzamos el esquema HTTPS si no estamos en entorno local.
+         * Esto elimina la pantalla de "Formulario no seguro" en Render.
+         */
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
